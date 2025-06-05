2d5ccd157c65d800aa2a407e348e59b7e493aff1
Aayan Akram <aaaka001@osloskolen.no>
2025-06-05 10:28:47 +0200

    initialize

---
+++ script.js
@@ -0,0 +1,20 @@
+const images = [
+    'bilfer/placeholder.png',
+    'bilfer/placeholder2.png',
+    'bilfer/placeholder3.png'
+];
+
+let current = 0
+
+const frontPage = document.querySelector('.frontPage');
+
+frontPage.style.backgroundImage = `url(${images[current]})`;
+
+function changeSlide(direction) {
+    current = (current + direction + images.length) % images.length;
+    frontPage.style.backgroundImage = `url('${images[current]}')`;
+    images.forEach(src => {
+        const img = new Image();
+        img.src = src;
+    });
+}
