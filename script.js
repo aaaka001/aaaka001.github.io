f46962a3eb8c6989f00e8db4f6f86f78ff5f9a7c
Aayan Akram <aaaka001@osloskolen.no>
2025-06-11 13:33:12 +0200

    Push

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 const images = [
     'bilder/gruppeBilde.JPG',
-    'bilder/photoshopOPPGAVE.png',
+    '',
     'bilder/placeholder3.png'
 ];
 
 let current = 0
 
 const frontPage = document.querySelector('.frontPage');
+
+const openBtn = document.getElementById('openPopup');
+const closeBtn = document.getElementById('closePopup');
+const popup = document.getElementById('videoPopup');
+const video = document.getElementById('videoPlayer');
+
+openBtn.addEventListener('click', () => {
+    popup.style.display = 'block';
+    video.play();
+});
+
+closeBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+    video.pause();
+    video.currentTime = 0; // Reset video to start
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === popup) {
+        popup.style.display = 'none';
+        video.pause();
+        video.currentTime = 0; // Reset video to start
+    }
+});
+
+
 
 frontPage.style.backgroundImage = `url(${images[current]})`;
 
